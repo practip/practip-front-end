f0d6456f849c6ad908abb7910d3016ccdb237ad7
Josh Lee <joshswl@gmail.com>
2017-04-05 14:40:13 -0500

    set up routes

--- app/router.js
+++ app/router.js
@@ -7,9 +7,20 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('instrument-slug', { path: '/instrument-slug' }, function() {
-    this.route('challenge-id');
-  })
+  this.route('challenge', function() {
+    this.route('create');
+    this.route('edit');
+    this.route('video');
+  });
+  this.route('dashboard');
+  this.route('instrument');
+  this.route('login', function() {
+    this.route('register');
+  });
+
+  this.route('user-profile', function() {
+    this.route('edit');
+  });
 });
 
 export default Router;
