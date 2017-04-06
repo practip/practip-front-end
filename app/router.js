9b5d8ec01123da474f4a83359c8065d099f329af
Caleb Lee <caleb.gwl@gmail.com>
2017-04-06 15:27:53 -0500

    fixing head branch

--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import config from './config/environment';
-
+//is this working?
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
