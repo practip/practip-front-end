566f73682676cb6dcc6aef5cbabc80ef2b274353
Caleb Lee <caleb.gwl@gmail.com>
2017-04-10 10:04:27 -0500

    Fix routing and adapter

--- app/router.js
+++ app/router.js
@@ -9,8 +9,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('dashboard');
 
-  this.route ('instrument', { path: '/instrument' }, function() {
-    this.route('challenge', function() {
+  this.route ('instrument', { path: '/instruments' }, function() {
+    this.route('challenge', { path: '/:instrument_id' }, function() {
       this.route('new');
       this.route('edit');
       this.route('video');
