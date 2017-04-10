b501940ea0c427de2643b62bd0915e9ccd2b4f8d
Caleb Lee <caleb.gwl@gmail.com>
2017-04-10 10:09:00 -0500

    Fix small issue in route.js

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('dashboard');
 
-  this.route ('instrument', { path: '/instruments' }, function() {
+  this.route('instrument', { path: '/instruments' }, function() {
     this.route('challenge', { path: '/:instrument_id' }, function() {
       this.route('new');
       this.route('edit');
