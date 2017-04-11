a9288127b68e5c38d71d155b04826f1b4b842bbc
Caleb Lee <caleb.gwl@gmail.com>
2017-04-11 15:14:35 -0500

    Add challenge model

--- app/instrument/challenge/edit/controller.js
+++ app/instrument/challenge/edit/controller.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  async saveEdits(changeset) {
+    await changeset.save();
+
+    this.transitionToRoute('challenge');
+  }
 });
