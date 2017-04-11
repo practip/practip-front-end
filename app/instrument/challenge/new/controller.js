ef177bbbaa9381e6ade339735ff9591236e783e6
Caleb Lee <caleb.gwl@gmail.com>
2017-04-11 15:28:48 -0500

    write action in challenge/new/controller

--- app/instrument/challenge/new/controller.js
+++ app/instrument/challenge/new/controller.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  formValues: {},
+
+  async submitChallenge() {
+    const challenge = this.store.createRecord('challenge', this.formValues);
+
+    await challenge.save();
+
+    this.set('formValues', {});
+    this.transitionToRoute('challenge');
+  }
 });
