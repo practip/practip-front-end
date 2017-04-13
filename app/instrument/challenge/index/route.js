bfc36c81cc572b3ea5ab7499afe7b782cf9de742
Caleb Lee <caleb.gwl@gmail.com>
2017-04-13 09:53:55 -0500

    Fix routing

--- app/instrument/challenge/index/route.js
+++ app/instrument/challenge/index/route.js
@@ -4,10 +4,4 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('challenge');
   },
-
-  // setupController(controller) {
-  //   this._super(...arguments);
-  //
-  //   controller.set('instrument', this.modelFor('instrument.challenge'));
-  // }
 });
--- app/router.js
+++ app/router.js
@@ -12,7 +12,7 @@ Router.map(function() {
   this.route('instrument', { path: '/instruments' }, function() {
     this.route('challenge', { path: '/:instrument_id' }, function() {
       this.route('new');
-      this.route('edit', { path: '/:challenge_id' });
+      this.route('edit');
       this.route('video', { path: '/:challenge_id'});
     });
 
