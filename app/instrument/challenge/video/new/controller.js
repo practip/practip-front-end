cf8d975626713548d43b0c61d70b9e67638b7568
Caleb Lee <caleb.gwl@gmail.com>
2017-04-18 10:23:29 -0500

    Fix routing issues

--- app/instrument/challenge/video/new/controller.js
+++ app/instrument/challenge/video/new/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import RedirectOnClose from '../redirect-on-close';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(RedirectOnClose, {
   formValues: {},
 
   actions: {
