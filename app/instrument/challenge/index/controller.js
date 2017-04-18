96f5204818f135db6516ca233123d327f3335859
Caleb Lee <caleb.gwl@gmail.com>
2017-04-18 16:29:51 -0500

    Add confirm delete modal for all objects

--- app/instrument/challenge/index/controller.js
+++ app/instrument/challenge/index/controller.js
@@ -2,29 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    confirmWithUser() {
-      var response = confirm('Are you sure you want to delete this challenge?');
-      return response;
-    },
-
     deleteChallenge(challenge) {
-      const response = confirmWithUser();
-      if(response) {
-        challenge.destroyRecord()
+      if(confirm('Are you sure you want to delete this challenge?')) {
+        challenge.destroyRecord();
       }
       else {
-        this.transitionToRoute('instrument.challenge');
+        this.transitionToRoute('instrument.challenge')
       }
     }
-    // deleteChallenge(challenge) {
-    //
-    //   if(alert = true) {
-    //     challenge.destroyRecord();
-    //   }
-    //
-    //   else {
-    //     this.transitionToRoute('instrument.challenge')
-    //   }
-    // }
   }
 });
--- app/instrument/challenge/video/comments/controller.js
+++ app/instrument/challenge/video/comments/controller.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 import RedirectOnClose from '../redirect-on-close';
 
 export default Ember.Controller.extend(RedirectOnClose, {
-
+  actions: {
+    deleteComment(comment) {
+      if(confirm('Are you sure you want to delete this comment?')) {
+        comment.destroyRecord();
+      }
+      else {
+        this.transitionToRoute('instrument.challenge.video.comments')
+      }
+    }
+  }
 });
