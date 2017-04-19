6f6eab62c118f7181705508e3b64b7a35a631cca
Caleb Lee <caleb.gwl@gmail.com>
2017-04-19 15:06:41 -0500

    Add exit buttons to modals

--- app/instrument/challenge/video/new/controller.js
+++ app/instrument/challenge/video/new/controller.js
@@ -5,16 +5,22 @@ export default Ember.Controller.extend(RedirectOnClose, {
   formValues: {},
 
   actions: {
-    submitVideo(changeset) {
-      changeset.save();
-      const [_, youtubeId] = this.formValues.videoUrl.match(/(?:https:\/\/www.youtube.com\/watch\?v=)(.+)/);
+    async submitVideo(changeset) {
+      await changeset.validate().then(() => {
+        if(changeset.get('isValid')) {
+          changeset.save();
+          const [_, youtubeId] = this.formValues.videoUrl.match(/(?:https:\/\/www.youtube.com\/watch\?v=)(.+)/);
 
-      const post = this.store.createRecord('post', this.formValues);
-      post.set('youtubeId', youtubeId);
-      post.set('challenge', this.model);
+          const post = this.store.createRecord('post', this.formValues);
+          post.set('youtubeId', youtubeId);
+          post.set('challenge', this.model);
 
-      post.save();
-      this.transitionToRoute('instrument.challenge.video');
+          post.save();
+          this.transitionToRoute('instrument.challenge.video');
+        } else {
+          alert('One or more fields are empty. Please fill in all forms to continue.');
+        }
+      })
     }
   }
 });
