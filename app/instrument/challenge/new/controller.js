32b7315bcaef7e6f53a858643d2332e7a38d714e
Caleb Lee <caleb.gwl@gmail.com>
2017-04-19 11:14:24 -0500

    Working edit validations for challenges and videos

--- app/instrument/challenge/new/controller.js
+++ app/instrument/challenge/new/controller.js
@@ -1,17 +1,26 @@
 import Ember from 'ember';
+import formValidations from '../../../validations/form';
 
 export default Ember.Controller.extend({
+  formValidations,
+
   formValues: {},
 
   actions: {
-    async submitChallenge() {
-      const challenge = this.store.createRecord('challenge', this.formValues);
-      challenge.set('instrument', this.instrument);
+    async submitChallenge(changeset) {
+      await changeset.validate().then(() => {
+        if(changeset.get('isValid')) {
+          const challenge = this.store.createRecord('challenge', this.formValues);
+          challenge.set('instrument', this.instrument);
 
-      await challenge.save();
+          challenge.save();
 
-      this.set('formValues', {});
-      this.transitionToRoute('instrument.challenge');
+          this.set('formValues', {});
+          this.transitionToRoute('instrument.challenge');
+        } else {
+          alert('One or more fields are empty. Please fill in all forms to continue.');
+        }
+      })
     }
   }
 });
